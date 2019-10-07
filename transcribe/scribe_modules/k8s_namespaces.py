3bd08ac8b3ffc32bbe0458e04eba5eb22f687b7e
dry923 <rzaleski@redhat.com>
2019-10-07 09:56:26 -0400

    Adding k8s_namespaces

---
+++ transcribe/scribe_modules/k8s_namespaces.py
@@ -0,0 +1,37 @@
+from . import ScribeModuleBaseClass
+from . lib.util import format_url
+
+import re as _re
+import sys
+import json
+
+
+class K8s_namespaces(ScribeModuleBaseClass):
+
+    def __init__(self, input_dict=None, module_name=None, host_name=None,
+                 input_type=None, scribe_uuid=None):
+        ScribeModuleBaseClass.__init__(self, module_name=module_name,
+                                       input_dict=input_dict,
+                                       host_name=host_name,
+                                       input_type=input_type,
+                                       scribe_uuid=scribe_uuid)
+        if input_dict:
+            self.value = self._parse(input_dict)
+
+    def __iter__(self):
+        for attr, value in self.__dict__.items():
+            yield attr, value
+
+    def _parse(self, items_full):
+        # To help with nested dictionaries that are in a format with
+        # escape characters we need to clean it up
+        new_items = json.dumps(items_full)
+        new_items = new_items.replace('\\n',"")
+        new_items = new_items.replace('\\'," ")
+        new_items = new_items.replace(" \"","\"")
+        new_items = new_items.replace(":\"{",": {")
+        new_items = new_items.replace("}\"}","}}")
+        if len(new_items) <= 1:
+            print("Error occured in processing k8s Namespaces data")
+            sys.exit(1)
+        return json.loads(new_items)
