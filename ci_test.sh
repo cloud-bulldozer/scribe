0ae48546f9d461f9421305d0902ed73b81c0f112
dry923 <rzaleski@redhat.com>
2019-10-10 17:37:50 -0400

    Adding testing against backpack for CI

--- ci_test.sh
+++ ci_test.sh
@@ -3,24 +3,21 @@
 # Prep the results.markdown file
 echo "Results for "$JOB_NAME > results.markdown
 echo "" >> results.markdown
-echo 'Module | Pass/Fail' >> results.markdown
-echo '-----|-------|' >> results.markdown
+echo 'Module | Instance | Kubernetes' >> results.markdown
+echo '-----|-------|------' >> results.markdown
 
 # Clone and prep Stockpile
 git clone https://github.com/cloud-bulldozer/stockpile.git
 
 cp stockpile/ci/all.yml stockpile/group_vars/all.yml
 
-# It's redundant to run it against kubernetes hosts atm so we remove them from the
-# hosts file
-cat stockpile/ci/hosts | grep -v ^# | sed -e '/\[kubernetes\]/,+1d' > stockpile/hosts
-
 sed -i 's/\/tmp\/stockpile.json/\/tmp\/stockpile_scribe.json/' stockpile/group_vars/all.yml
+sed -i 's/\/tmp\/container/\/tmp\/scribe_container/' stockpile/group_vars/all.yml
 
 cd stockpile
 
 echo "Running Stockpile"
-ansible-playbook -i hosts stockpile.yml -e kube_config=/root/.kube/config #--skip-tags=backpack_kube
+ansible-playbook -i ci/hosts stockpile.yml -e kube_config=/root/.kube/config
 
 cd ..
 
@@ -38,18 +35,38 @@ pip3 install -e .
 
 rc=0
 
+# Test against the local (instance) data
 python3 -m scribe -t stockpile -ip /tmp/stockpile_scribe.json > scribe_out
 scribe_rc=$?
 
 if [[ $scribe_rc -eq 1 ]]
 then
-  echo "Scribe failed to run. Exiting"
+  echo "Scribe failed to run checking instance output. Exiting"
   exit 1
 fi
 
-echo "Full scribe output:"
+# Test against against the backpack created data
+for cont_file in `ls /tmp/scribe_container/backpack*`
+do
+  echo $cont_file
+  python3 -m scribe -t stockpile -ip $cont_file > scribe_cont_out
+  scribe_cont_rc=$?
+
+  if [[ $scribe_cont_rc -eq 1 ]]
+  then
+    echo "Scribe failed to run checking container output. Exiting"
+    exit 1
+  fi
+done
+
+
+echo "Full scribe instance output:"
 cat scribe_out
 echo ""
+echo ""
+echo ""
+echo "Full scribe container output:"
+cat scribe_cont_out
 echo ""
 echo ""
 echo "Scribe Module Results:"
@@ -57,21 +74,32 @@ echo "Scribe Module Results:"
 for module in $module_list
 do
   grep $module" is not part" scribe_out
-  my_rc=$?
+  my_inst_rc=$?
+  grep $module" is not part" scribe_cont_out
+  my_cont_rc=$?
 
-  if [ $my_rc -eq 1 ]
+  if [ $my_inst_rc -eq 1 ]
   then
-    echo $module": Passed"
-    echo $module"|Pass" >> results.markdown
+    echo -n $module": Passed Instance"
+    echo -n $module"|Pass" >> results.markdown
   else
-    echo $module": Failed"
-    echo $module"|Fail" >> results.markdown
+    echo -n $module": Failed Instance"
+    echo -n $module"|Fail" >> results.markdown
+    rc=1
+  fi
+  if [ $my_cont_rc -eq 1 ]
+  then
+    echo " | Passed Kubernetes"
+    echo "|Pass" >> results.markdown
+  else
+    echo " |  Failed Kubernetes"
+    echo "|Fail" >> results.markdown
     rc=1
   fi
 done
 
 deactivate
 
-rm -rf stockpile ci_test 
+rm -rf stockpile ci_test /tmp/scribe_container
 
 exit $rc
