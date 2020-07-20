d07ca4f31187aee2e582abe201fd17e337ac5b1f
Raúl Sevilla <rsevilla@redhat.com>
2020-07-20 15:39:55 +0200

    Add ocp cluster install (#28)
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>
    
    Co-authored-by: Aakarsh Gopi <aakarsh.g2012@gmail.com>

---
+++ transcribe/scribe_modules/ocp_install_config.py
@@ -0,0 +1,16 @@
+from . import ScribeModuleBaseClass
+
+
+class Ocp_install_config(ScribeModuleBaseClass):
+
+    def __init__(self, input_dict=None, module_name=None, host_name=None,
+                 input_type=None, scribe_uuid=None):
+        ScribeModuleBaseClass.__init__(self, module_name=module_name,
+                                       input_dict=input_dict,
+                                       host_name=host_name,
+                                       input_type=input_type,
+                                       scribe_uuid=scribe_uuid)
+
+    def parse(self):
+        self._dict['value'] = self._input_dict
+        yield self._dict
