10ef829ed889690172269446fec7a4ae25c9db46
Keshav Maheshwari <kmaheshw@redhat.com>
2020-08-18 18:24:39 -0400

    final clean up

--- transcribe/scribe_modules/nvidia_smi.py
+++ transcribe/scribe_modules/nvidia_smi.py
@@ -1,16 +1,11 @@
 from . import ScribeModuleBaseClass
-
-import sys
-
-
 from . lib.util import validate_length
 
-import json
+import sys
 import xmltodict
-import pprint
 
 class Nvidia_smi(ScribeModuleBaseClass):
-
+    
     def __init__(self, input_dict=None, module_name=None, host_name=None,
                  input_type=None, scribe_uuid=None):
         ScribeModuleBaseClass.__init__(self, module_name=module_name,
@@ -18,7 +13,6 @@ class Nvidia_smi(ScribeModuleBaseClass):
                                        host_name=host_name,
                                        input_type=input_type,
                                        scribe_uuid=scribe_uuid)
-
 
     def parse(self):
         validate_length(len(self._input_dict['xml']), self.module)
